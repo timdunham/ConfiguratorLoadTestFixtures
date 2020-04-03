f9c848b433c2c626547b5fc6f6b83111b5e9db39
Tim Dunham <tim.dunham@infor.com>
2020-04-03 09:51:07 -0400

    update default header id for v2 configurations

--- src/ConfigurationV2.cs
+++ src/ConfigurationV2.cs
@@ -1,12 +1,10 @@
-using System.Net.Http;
-using System.Text;
 using Zoxive.HttpLoadTesting.Framework.Http;
 
 namespace Infor.CPQ.ConfiguratorLoadTestFixtures
 {
     public class ConfigurationV2: Configuration
     {
-        internal override string HeaderId => "Simulator";
+        internal override string HeaderId => "Fixture-v2";
 
         public ConfigurationV2(IUserLoadTestHttpClient userLoadTestHttpClient, string tenant, string rulesetNamespace, string ruleset)
             :base(userLoadTestHttpClient, "api/v4", tenant, rulesetNamespace, ruleset)
